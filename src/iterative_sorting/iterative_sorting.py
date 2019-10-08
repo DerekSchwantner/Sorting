04659283140186fb48fb87053482a0f1beb5c5a8
Derek S <dschwan21@me.com>
2019-10-07 21:58:54 -0700

    first iteration of selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,19 +1,19 @@
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
+        for n in range(i, len(arr)):
+            if arr[n] < arr[smallest_index]:
+                print(f"swapped: {arr[smallest_index]} for {arr[n]}")
+                smallest_index = n
 
 
         # TO-DO: swap
-
-
-
+            arr[smallest_index],arr[cur_index] = arr[cur_index],arr[smallest_index]
 
     return arr
 
@@ -28,3 +28,10 @@ def bubble_sort( arr ):
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+unsrted_nums = [44, 53, 2, 1, 600, 76, 23, 90]
+
+print(selection_sort(unsrted_nums))
+
+
+
